52e29d447b26755a5ceee49b0b641db02999b1b1
Alan Gibson <alangibson27@gmail.com>
2014-12-15 21:16:39 +0000

    Changed commit message used by deploy-hugo.

--- bin/deploy-hugo.sh
+++ bin/deploy-hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying public site updates to GitHub...\033[0m"
 
 # Build the project. 
 hugo -t hinatabokko 

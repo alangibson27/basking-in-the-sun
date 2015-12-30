d06cf9013e614ff7f5bd4c0493b9f1223f1d3988
Alan Gibson <alangibson27@gmail.com>
2015-12-30 23:09:13 +0000

    Introduced shell scripts to make it easier to test a local version of the site

--- Site/bin/test-hugo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PID=`lsof -t -i:1313 -sTCP:LISTEN`
-if [ $PID ]; then
-  kill $PID
-fi
-
-hugo server --destination=.hugolocal --buildDrafts=true --theme=hinatabokko &
-
---
+++ localSite.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+hugo server --source=Site --destination=.hugolocal --buildDrafts=true --theme=hinatabokko
